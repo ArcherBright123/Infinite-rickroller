3b3360d03e66b24d5ce76488e8d3ae6d029f2b3b
ArcherBright123 <125842438+ArcherBright123@users.noreply.github.com>
2023-04-04 11:05:41 +1000

    Add files via upload

---
+++ Infinite Rickroller.py
@@ -0,0 +1,7 @@
+import webbrowser
+x = 10000000000000000
+for i in range(x):
+    def infinite_Rickroll():
+        url =  "https://www.youtube.com/watch?v=mx86-rTclzA"
+        webbrowser.open_new(url)
+    infinite_Rickroll()
